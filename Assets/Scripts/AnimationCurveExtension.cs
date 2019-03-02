b8f360edd63e3894c9c2e77ecd18f3d66c53afef
DantesGearbox <mads.engberg@hotmail.com>
2019-03-02 14:36:16 +0100

    initial commit

---
+++ Assets/Scripts/AnimationCurveExtension.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public static class AnimationCurveExtension {
+
+	//This function presupposed that the values of the animation curve are always rising
+	public static float ReverseEvaluate(this AnimationCurve animCurve, float value) {
+
+		float min = 0;
+		float max = 1.0f;
+		
+		float currentTime = 0;
+		float currentValue = 0;
+
+		for(int i = 0; i < 100; i++) {
+			currentTime = (min + max) / 2;
+			currentValue = animCurve.Evaluate(currentTime);
+
+			if (currentValue > value) {
+				max = currentTime;
+			}
+
+			if (currentValue < value) {
+				min = currentTime;
+			}
+
+			if (animCurve.Evaluate(currentTime) == value) {
+				break;
+			}
+		}
+		
+		return currentTime;
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	/* Movement:
+	 * Simple rules that add up to complex behavior
+	 * Keep things seperate
+	 * Make sure to keep things understandable
+	 */
+
+	private Rigidbody2D rb;
+
+	public float gravity = -10;
+	public float acceleration = 10;
+	public float jumpForce = 100;
+
+	public bool onGround = false;
+
+	public AnimationCurve curve;
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		onGround = true;
+	}
+
+	private void OnCollisionExit2D(Collision2D collision) {
+		onGround = false;
+	}
+
+	private void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	private void Update() {
+
+		if (Input.GetKey(KeyCode.A)) {
+			//rb.AddForce(new Vector2(-acceleration, 0));
+			rb.velocity -= new Vector2(AnimationCurveExtension.ReverseEvaluate(curve, rb.velocity.x), 0);
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			rb.velocity += new Vector2(AnimationCurveExtension.ReverseEvaluate(curve, rb.velocity.x), 0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rb.AddForce(new Vector2(0, jumpForce));
+		}
+
+		if (!onGround) {
+			rb.AddForce(new Vector2(0, gravity));
+		}
+	}
+}
