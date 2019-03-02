9825e440983f62468862c5a26a9f1c5d29b439da
DantesGearbox <mads.engberg@hotmail.com>
2019-03-02 23:08:48 +0100

    Animation curves

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,10 +12,17 @@ public class Movement : MonoBehaviour {
 
 	private Rigidbody2D rb;
 
-	public float gravity = -10;
-	public float acceleration = 10;
-	public float jumpForce = 100;
+	private float gravity = -10;
+	private float acceleration = 10;
+	private float maxGroundVelocity = 5;
+	private float jumpForce = 100;
 
+	public KeyCode leftButton = KeyCode.A;
+	public KeyCode rightButton = KeyCode.D;
+	public KeyCode jumpButton = KeyCode.Space;
+	private bool inputLeft;
+	private bool inputRight;
+	private bool inputJump;
 	public bool onGround = false;
 
 	public AnimationCurve curve;
@@ -35,17 +42,46 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	private void Update() {
 
-		if (Input.GetKey(KeyCode.A)) {
-			//rb.AddForce(new Vector2(-acceleration, 0));
-			rb.velocity -= new Vector2(AnimationCurveExtension.ReverseEvaluate(curve, rb.velocity.x), 0);
+		inputLeft = Input.GetKey(leftButton);
+		inputRight = Input.GetKey(rightButton);
+		inputJump = Input.GetKeyDown(jumpButton);
+
+	}
+
+
+	//Bug: When at full speed it is impossible to turn around
+	//It happens because the deltaVelocity becomes 0 since no more velocity has to be added
+
+	//The fix shouldn't just turn the velocity around, but should also make sure that the proper acceleration is being applied
+
+	private void FixedUpdate() {
+
+		if(inputLeft || inputRight) {
+			float value = Mathf.Abs(rb.velocity.x / maxGroundVelocity);
+
+			float currentTime = AnimationCurveExtension.ReverseEvaluate(curve, value);
+			float newTime = currentTime + Time.deltaTime;
+			float newVelocity = curve.Evaluate(newTime) * maxGroundVelocity;
+			float velocityDelta = newVelocity - Mathf.Abs(rb.velocity.x);
+			Debug.Log("ReverseGetTime: " + currentTime + ", currentVelocity: " + rb.velocity.x + ", newVelocity: " + newVelocity + ", velocity delta: " + velocityDelta);
+
+			Vector2 addVector = new Vector2(velocityDelta, 0);
+
+			if (inputLeft) {
+				rb.velocity -= addVector;
+			}
+
+			if (inputRight) {
+				rb.velocity += addVector;
+			}
 		}
 
-		if (Input.GetKey(KeyCode.D)) {
-			rb.velocity += new Vector2(AnimationCurveExtension.ReverseEvaluate(curve, rb.velocity.x), 0);
-		}
+		//Clamp horizontal speed
+		float clampedXSpeed = Mathf.Clamp(rb.velocity.x, -maxGroundVelocity, maxGroundVelocity);
+		rb.velocity = new Vector2(clampedXSpeed, rb.velocity.y);
 
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			rb.AddForce(new Vector2(0, jumpForce));
+		if (inputJump && onGround) {
+			rb.AddForce(new Vector2(0, jumpForce));	
 		}
 
 		if (!onGround) {
